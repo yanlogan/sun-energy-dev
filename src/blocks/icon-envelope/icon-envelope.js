7f93f1050c8d3c3bef29ed9f016a5ea392c6ae59
Yan Logan <yanlogans@gmail.com>
2019-04-26 21:52:48 +0300

    created icon-envelope block

---
+++ src/blocks/icon-envelope/icon-envelope.js
@@ -0,0 +1,13 @@
+modules.define('icon-envelope', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
