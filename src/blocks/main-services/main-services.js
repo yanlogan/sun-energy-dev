62b51aa47011604131cc055b0a32fd6377c1881f
Yan Logan <yanlogans@gmail.com>
2019-05-01 23:34:58 +0300

    added tabs and slider for main-services block

--- src/blocks/main-services/main-services.js
+++ src/blocks/main-services/main-services.js
@@ -1,13 +1,65 @@
-modules.define('main-services', ['i-bem-dom'], function(provide, bemDom) {
+var tabs = document.querySelectorAll('.main-services__tab');
+var tabPanels = document.querySelectorAll('.main-services-tabcontent');
+var prevButton = document.querySelector('.main-services-prev');
+var nextButton = document.querySelector('.main-services-next');
+var activeTab = document.querySelector('.main-services__tab_active');
+var activePanel = document.querySelector('.main-services-tabcontent_active');
+var current = 0;
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
+const resetTabs = () => {
+	tabs.forEach(node => {
+		node.classList.remove('main-services__tab_active');
+	});
+	tabPanels.forEach(node => {
+		node.classList.remove('main-services-tabcontent_active');
+		node.style.display = 'none';
+	});
+};
 
+const addTabClickHandler = (tab, tabPanel, index) => {
+	tab.addEventListener('click', (e) => {
+		e.preventDefault();
+		resetTabs();
+		tab.classList.add('main-services__tab_active');
+		tabPanel.classList.add('main-services-tabcontent_active');
+		tabPanel.style.display = 'block';
+		current = index;
+	});
+};
+
+const showPanel = () => {
+	tabs[0].classList.add('main-services__tab_active');
+	tabPanels[0].classList.add('main-services-tabcontent_active');
+	tabPanels[0].style.display = 'block';
+	for (var i = 0; i < tabs.length; i++) {
+		addTabClickHandler(tabs[i], tabPanels[i], i);
+	}
+};
+
+showPanel();
+
+const slideNext = () => {
+	resetTabs();
+	tabs[current + 1].classList.add('main-services__tab_active');
+	tabPanels[current + 1].classList.add('main-services-tabcontent_active');
+	tabPanels[current + 1].style.display = 'block';
+	current++;
+};
+
+const slidePrev = () => {
+	resetTabs();
+	tabs[current - 1].classList.add('main-services__tab_active');
+	tabPanels[current - 1].classList.add('main-services-tabcontent_active');
+	tabPanels[current - 1].style.display = 'block';
+	current--;
+};
+
+nextButton.addEventListener('click', () => {
+	if (current === tabs.length - 1) current = -1;
+	slideNext();
+});
+
+prevButton.addEventListener('click', () => {
+	if (current === 0) current = tabs.length;
+	slidePrev();
 });
--- src/js/import/blocks.js
+++ src/js/import/blocks.js
@@ -1,3 +1,4 @@
 import "%blocks%/header/header.js";
 import "%blocks%/footer/footer.js";
 import "%blocks%/main/main.js";
+import "%blocks%/main-services/main-services.js";
