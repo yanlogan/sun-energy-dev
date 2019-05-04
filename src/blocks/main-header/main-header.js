8e93a93dd664ed1cadbc7c1ee6b2f4281ce3eb3d
Yan Logan <yanlogans@gmail.com>
2019-05-04 18:21:59 +0300

    fixed main services slider

--- src/blocks/main-header/main-header.js
+++ src/blocks/main-header/main-header.js
@@ -1,33 +1,46 @@
-var sliderButtons = document.querySelectorAll('.header-slider__btn');
-var slides = document.querySelectorAll('.header-slider__slide');
+import "../../../node_modules/jquery/dist/jquery.min.js";
+import $ from "jquery";
+// import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../../../node_modules/bootstrap/js/dist/tab.js";
+import "../../../node_modules/slick-carousel/slick/slick.min.js";
 
-var currentMainSlider = 0;
+// var sliderButtons = document.querySelectorAll(".header-slider__btn");
+// var slides = document.querySelectorAll(".header-slider__slide");
 
-const resetMainSlider = () => {
-	sliderButtons.forEach(node => {
-		node.classList.remove('header-slider__btn_active');
+// const resetMainSlider = () => {
+// 	sliderButtons.forEach(node => {
+// 		node.classList.remove("header-slider__btn_active");
+// 	});
+// 	slides.forEach(node => {
+// 		node.style.display = "none";
+// 	});
+// };
+
+// const addSliderClickHandler = (sliderButton, slide) => {
+// 	sliderButton.addEventListener("click", (e) => {
+// 		e.preventDefault();
+// 		resetMainSlider();
+// 		sliderButton.classList.add("header-slider__btn_active");
+// 		slide.style.display = "block";
+// 	});
+// };
+
+// const showSlide = () => {
+// 	sliderButtons[0].classList.add("header-slider__btn_active");
+// 	slides[0].style.display = "block";
+// 	for (var i = 0; i < sliderButtons.length; i++) {
+// 		addSliderClickHandler(sliderButtons[i], slides[i]);
+// 	}
+// };
+
+// showSlide();
+
+$(document).ready(function(){
+	$(".header-slider .container").slick({
+		autoplay: true,
+		autoplaySpeed: 5000,
+		arrows: false,
+		dots: true,
+		dotsClass: "header-slides"
 	});
-	slides.forEach(node => {
-		node.style.display = 'none';
-	});
-};
-
-const addSliderClickHandler = (sliderButton, slide, index) => {
-	sliderButton.addEventListener('click', (e) => {
-		e.preventDefault();
-		resetMainSlider();
-		sliderButton.classList.add('header-slider__btn_active');
-		slide.style.display = 'block';
-		currentMainSlider = index;
-	});
-};
-
-const showSlide = () => {
-	sliderButtons[0].classList.add('header-slider__btn_active');
-	slides[0].style.display = 'block';
-	for (var i = 0; i < sliderButtons.length; i++) {
-		addSliderClickHandler(sliderButtons[i], slides[i], i);
-	}
-};
-
-showSlide();
+});
--- src/blocks/main-services/main-services.js
+++ src/blocks/main-services/main-services.js
@@ -1,63 +1,28 @@
-var tabs = document.querySelectorAll('.main-services__tab');
-var tabPanels = document.querySelectorAll('.main-services-tabcontent');
-var prevButton = document.querySelector('.main-services-prev');
-var nextButton = document.querySelector('.main-services-next');
-var currentServiceTab = 0;
+import "../../../node_modules/jquery/dist/jquery.min.js";
+import $ from "jquery";
+import "../../../node_modules/bootstrap/js/dist/tab.js";
+import "../../../node_modules/bootstrap/js/dist/carousel.js";
 
-const resetTabs = () => {
-	tabs.forEach(node => {
-		node.classList.remove('main-services__tab_active');
-	});
-	tabPanels.forEach(node => {
-		node.classList.remove('main-services-tabcontent_active');
-		node.style.display = 'none';
-	});
-};
-
-const addTabClickHandler = (tab, tabPanel, index) => {
-	tab.addEventListener('click', (e) => {
-		e.preventDefault();
-		resetTabs();
-		tab.classList.add('main-services__tab_active');
-		tabPanel.classList.add('main-services-tabcontent_active');
-		tabPanel.style.display = 'block';
-		currentServiceTab = index;
-	});
-};
-
-const showPanel = () => {
-	tabs[0].classList.add('main-services__tab_active');
-	tabPanels[0].classList.add('main-services-tabcontent_active');
-	tabPanels[0].style.display = 'block';
-	for (var i = 0; i < tabs.length; i++) {
-		addTabClickHandler(tabs[i], tabPanels[i], i);
-	}
-};
-
-showPanel();
-
-const slideNext = () => {
-	resetTabs();
-	tabs[currentServiceTab + 1].classList.add('main-services__tab_active');
-	tabPanels[currentServiceTab + 1].classList.add('main-services-tabcontent_active');
-	tabPanels[currentServiceTab + 1].style.display = 'block';
-	currentServiceTab++;
-};
-
-const slidePrev = () => {
-	resetTabs();
-	tabs[currentServiceTab - 1].classList.add('main-services__tab_active');
-	tabPanels[currentServiceTab - 1].classList.add('main-services-tabcontent_active');
-	tabPanels[currentServiceTab - 1].style.display = 'block';
-	currentServiceTab--;
-};
-
-nextButton.addEventListener('click', () => {
-	if (currentServiceTab === tabs.length - 1) currentServiceTab = -1;
-	slideNext();
+$(document).ready( function() {
+	$('#servicesCarousel').carousel({
+		interval:   4000
 });
 
-prevButton.addEventListener('click', () => {
-	if (currentServiceTab === 0) currentServiceTab = tabs.length;
-	slidePrev();
+var clickEvent = false;
+$('#servicesCarousel').on('click', '.nav a', function() {
+		clickEvent = true;
+		$('.nav a').removeClass('main-services__tab_active');
+		$(this).addClass('main-services__tab_active');		
+}).on('slid.bs.carousel', function(e) {
+	if(!clickEvent) {
+		var count = $('.nav').children().length -1;
+		var current = $('.nav a.main-services__tab_active');
+		current.removeClass('main-services__tab_active').parent().next().children().addClass('main-services__tab_active');
+		var id = parseInt(current.parent().data('slide-to'));
+		if(count == id) {
+			$('.nav a').first().addClass('main-services__tab_active');	
+		}
+	}
+	clickEvent = false;
+});
 });
