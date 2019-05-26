29e31a4c98c2131425de16fcceb7ca3aae2d450a
Yan Logan <yanlogans@gmail.com>
2019-05-26 16:51:44 +0300

    fixed products slider

--- src/blocks/main-products/main-products.js
+++ src/blocks/main-products/main-products.js
@@ -2,52 +2,55 @@
 import "../../../node_modules/jquery/dist/jquery.min.js";
 import * as $ from "jquery";
 import "../../../node_modules/bootstrap/js/dist/tab.js";
-import "../../../node_modules/bootstrap/js/dist/carousel.js";
+import "../../../node_modules/slick-carousel/slick/slick.min.js";
 
-import "../../../node_modules/owl.carousel/dist/owl.carousel.min.js";
+$(document).ready(function() {
 
-$(document).ready(function(){
-	var owl = $(".main-products .owl-carousel");
-	owl.owlCarousel({
-		loop:false,
-		margin:10,
-		nav: true,
-		navSpeed: 1000,
-		dots: false,
-		// navContainerClass: "main-products-nav"
-	})
-		.on("changed.owl.carousel", function(e){
-			console.log("Carousel state changed");
+	$(".slider").each(function() {
+		const slider = $(this);
+		var fired = false;
+		slider.slick({
+			infinite: false,
+			autoplay: false,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			appendArrows: false,
+		}).on("afterChange", function (event, slick, currentSlide) {
 			var currentTab = $(".main-products-tabs.nav a.active");
 			var nextTab = currentTab.parent().next().children();
-			console.log("Next tab: " + nextTab.html());
-			// if it's not the last tab
-			if (nextTab.length) { //!currentTab.parent().is(".main-products-tabs:last-child"
-			// then if the last item is shown and there's nothing left to scroll
-				if (e.item.index === e.item.count - e.page.size) {
-					// if we click next
-					$(".owl-next.disabled").one("click", () => {
-						console.log("Disabled button clicked");
-						// carousel resets so if we switch back to this tab, items start from 0 again
-						owl.trigger("to.owl.carousel", [0]);
-						console.log("Carousel reset to 0 item");
-						// make next tab active
-						// main-products__tab_active just styles the active tab color and underline
-						currentTab.removeClass("main-products__tab_active");
-						nextTab.addClass("main-products__tab_active");
-						nextTab.tab("show");	
+			if (!fired) {
+				slider.siblings(".main-products-next").one("click", () => {
+					if (nextTab.length) {
+						if (currentSlide === slick.slideCount - slick.options.slidesToShow) {
+							currentTab.removeClass("main-products__tab_active");
+							nextTab.addClass("main-products__tab_active");
+							nextTab.tab("show");
+							fired = true;
+						}
 					}
-					);
-					
-				}
+				});
 			}
+			if(currentSlide === 0) {
+				slider.siblings(".main-products-prev").hide();
+			}
+			else {
+				slider.siblings(".main-products-prev").show();
+			}  
 		});
-
-	// this code just styles active tab on click on it, not on carousel
-	$(".main-products-content")
-		.on("click", ".main-products-tabs.nav a", function() {
-			$(".nav a").removeClass("main-products__tab_active");
-			$(this).addClass("main-products__tab_active");		
+		slider.siblings(".main-products-prev").hide();
+		slider.siblings(".main-products-prev").click(() => {
+			slider.slick("slickPrev");
 		});
-	
+		slider.siblings(".main-products-next").click(() => {
+			slider.slick("slickNext");
+		});
+		$(".main-products-tabs").on("show.bs.tab", () => {
+			slider.slick("slickGoTo", 0);
+			fired = false;
+		});
+	});
+	$(".main-products-content").on("click", ".main-products-tabs.nav a", function() {
+		$(".nav a").removeClass("main-products__tab_active");
+		$(this).addClass("main-products__tab_active");	
+	});
 });
