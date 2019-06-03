b33e28ee072931ddb41749a4a181ccda9fac883a
Yan Logan <yanlogans@gmail.com>
2019-06-03 17:55:11 +0300

    added responsive styles to main services section

--- src/blocks/main-services/main-services.js
+++ src/blocks/main-services/main-services.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 import "../../../node_modules/jquery/dist/jquery.min.js";
 import $ from "jquery";
 import "../../../node_modules/slick-carousel/slick/slick.min.js";
@@ -8,7 +9,18 @@ $(document).ready( function() {
 	slider.slick({
 		autoplay: false,
 		infinite: false,
-		appendArrows: false,
+		appendArrows: slider.parent(),
+		prevArrow: `<span class="main-services-prev"><svg class="main-services-prev-icon" xmlns="http://www.w3.org/2000/svg" width="38" height="71" viewBox="0 0 38 71"><g><g><path fill="#fff" d="M.015 68.17l2.815 2.842L38.01 35.5 2.83-.012.015 2.83 32.382 35.5z"/></g></g></svg></span>`,
+		nextArrow: `<span class="main-services-next"><svg class="main-services-next-icon" xmlns="http://www.w3.org/2000/svg" width="38" height="71" viewBox="0 0 38 71"><g><g><path fill="#fff" d="M.015 68.17l2.815 2.842L38.01 35.5 2.83-.012.015 2.83 32.382 35.5z"/></g></g></svg></span>`,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				appendArrows: false,
+			}
+		}, {
+			breakpoint: 300,
+			settings: "unslick"
+		}]
 	}).on("afterChange", function(event, slick, currentSlide) {
 		$(".main-services__tab").removeClass("main-services__tab_active");
 		$(`a[data-slide=${currentSlide}]`).addClass("main-services__tab_active");
@@ -25,18 +37,12 @@ $(document).ready( function() {
 			slider.siblings(".main-services-next").show();
 		}   
 	});
+	slider.siblings(".main-services-prev").hide();
 	$("a[data-slide]").click(function(e) {
 		$(".main-services__tab").removeClass("main-services__tab_active");
 		$(this).addClass("main-services__tab_active");
 		e.preventDefault();
 		var slideId = $(this).data("slide");
 		slider.slick("slickGoTo", slideId);
-	});
-	slider.siblings(".main-services-prev").hide();
-	slider.siblings(".main-services-prev").click(() => {
-		slider.slick("slickPrev");
-	});
-	slider.siblings(".main-services-next").click(() => {
-		slider.slick("slickNext");
 	});
 });
