9c4ca0ac7af5aab82053018045380524e061d6b8
Yan Logan <yanlogans@gmail.com>
2019-06-03 17:51:27 +0300

    imported modal elements

--- src/js/import/blocks.js
+++ src/js/import/blocks.js
@@ -1,8 +1,12 @@
 import "%blocks%/header/header.js";
+import "%blocks%/burger/burger.js";
 import "%blocks%/main-header/main-header.js";
 import "%blocks%/footer/footer.js";
 import "%blocks%/main/main.js";
 import "%blocks%/main-services/main-services.js";
 import "%blocks%/main-products/main-products.js";
 import "%blocks%/products/products.js";
+import "%blocks%/modal-products/modal-products.js";
+import "%blocks%/footer-contacts/footer-contacts.js";
 import "%blocks%/modal/modal.js";
+import "%blocks%/modal-contacts/modal-contacts.js";
